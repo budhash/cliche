5f173a2c2a7f01ca5b481b8bfc9cf7f22fbd45e8
Budhaditya Das <budha@apple.com>
2015-05-25 11:45:01 -0700

    initial migration and mavenization

---
+++ README.md
@@ -0,0 +1,7 @@
+# Cliche Command-Line Shell
+Cliche is a small Java library enabling really simple creation of interactive command-line user interfaces.
+
+It uses metadata and Java Reflection to determine which class methods should be exposed to end user and to provide info for user. Therefore all information related to specific command is kept in only one place: in annotations in method's header. User don't have to organize command loop, write complicated parsers/converters for primitive types, though he can implement custom converters when needed.
+
+## Project History
+The original project was developed by Anton Grigoryev and the code can be found at https://code.google.com/p/cliche/
---
+++ src/test/java/shell/cliche/OutputConversionEngineTest.java
@@ -0,0 +1,64 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package shell.cliche;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import shell.cliche.OutputConversionEngine;
+import shell.cliche.OutputConverter;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author ASG
+ */
+public class OutputConversionEngineTest {
+
+    @Before
+    public void setUp() {
+        converter = new OutputConversionEngine();
+        converter.addDeclaredConverters(this);
+    }
+
+    OutputConversionEngine converter;
+
+    public static final OutputConverter[] CLI_OUTPUT_CONVERTERS = {
+        new OutputConverter() {
+            public Object convertOutput(Object toBeFormatted) {
+                if (toBeFormatted instanceof String) {
+                    return String.format("(%s)", (String)toBeFormatted);
+                } else {
+                    return null;
+                }
+            }
+        },
+        new OutputConverter() {
+            public Object convertOutput(Object toBeFormatted) {
+                if (toBeFormatted instanceof String) {
+                    return String.format("[%s]", (String)toBeFormatted);
+                } else {
+                    return null;
+                }
+            }
+        },
+    };
+
+
+    /**
+     * This method tests the order of applying converters, as other methods of
+     * OutputConversionEngine are rather trivial.
+     */
+    @Test
+    public void testConvertOutput() {
+        System.out.println("convertOutput");
+        String toBeConverted = "a";
+        String expected = "([a])";
+        assertEquals(expected, converter.convertOutput(toBeConverted));
+    }
+
+
+}
